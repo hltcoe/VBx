a30a86c24423ce7d7859458432d09c0f0c6da569
Kiran Karra <Kiran.Karra@jhuapl.edu>
2021-02-03 09:53:01 -0500

    readded uge commands for callhome eval

--- VBx/extract.sh
+++ VBx/extract.sh
@@ -18,12 +18,24 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -pv $OUT_DIR
 
 TASKFILE=$OUT_DIR/xv_task
+UGE_TASKFILE=$OUT_DIR/uge_xv_task.sh
 rm -f $TASKFILE
+rm -f $UGE_TASKFILE
+
+echo "#!/bin/bash" >> $UGE_TASKFILE
+echo ". /etc/profile.d/modules.sh" >> $UGE_TASKFILE
+echo "source deactivate" >> $UGE_TASKFILE
+echo "source activate xvec" >> $UGE_TASKFILE
+printf "flist=(" >> $UGE_TASKFILE
+
 
 mkdir -p $OUT_DIR/lists $OUT_DIR/xvectors $OUT_DIR/segments
 while IFS= read -r line; do
 	mkdir -p "$(dirname $OUT_DIR/lists/$line)"
     grep $line $FILE_LIST > $OUT_DIR/lists/$line".txt"
+
+    printf "$line " >> $UGE_TASKFILE
+
     OUT_ARK_FILE=$OUT_DIR/xvectors/$line.ark
     OUT_SEG_FILE=$OUT_DIR/segments/$line
     mkdir -p "$(dirname $OUT_ARK_FILE)"
@@ -34,3 +46,12 @@ while IFS= read -r line; do
     	echo "python $DIR/predict.py --seg-len 144 --seg-jump 24 --model $MODEL --weights $WEIGHTS --gpus= $MDL_WEIGHTS --ndim 64 --embed-dim $EMBED_DIM --in-file-list $OUT_DIR/lists/$line".txt" --in-lab-dir $LAB_DIR --in-wav-dir $WAV_DIR --out-ark-fn $OUT_ARK_FILE --out-seg-fn $OUT_SEG_FILE" >> $TASKFILE
     fi
 done < $FILE_LIST
+
+printf ")\n\n" >> $UGE_TASKFILE
+
+# TODO: add GPU support
+echo "file_uge=\${flist[\$((\${SGE_TASK_ID}-1))]}" >> $UGE_TASKFILE
+echo "out_ark_file_uge=$OUT_DIR/xvectors/\${file_uge}.ark" >> $UGE_TASKFILE
+echo "out_seg_file_uge=$OUT_DIR/segments/\${file_uge}" >> $UGE_TASKFILE
+echo "python $DIR/predict.py --seg-len 144 --seg-jump 24 --model $MODEL --weights $WEIGHTS --gpus= $MDL_WEIGHTS --ndim 64 --embed-dim $EMBED_DIM --in-file-list $OUT_DIR/lists/\${file_uge}".txt" --in-lab-dir $LAB_DIR --in-wav-dir $WAV_DIR --out-ark-fn \$out_ark_file_uge --out-seg-fn \$out_seg_file_uge" >> $UGE_TASKFILE
+
